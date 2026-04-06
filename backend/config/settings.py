3f8635e80d35036308da938acff79b464fb57348
jo <yohanneshailegiorgis0@gmail.com>
2026-04-06 10:51:43 +0300

    add auto admin creation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -184,3 +184,23 @@ if not DEBUG and _ON_RENDER:
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = "DENY"
+
+
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def create_admin():
+    username = os.environ.get("ADMIN_USERNAME")
+    email = os.environ.get("ADMIN_EMAIL")
+    password = os.environ.get("ADMIN_PASSWORD")
+
+    if username and password:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+
+try:
+    create_admin()
+except Exception as e:
+    print("Admin creation skipped:", e)
